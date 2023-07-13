5169b282fc9fdcf79a41b35881fe446061dc5511
Subramanya1702 <subramanyak1997@gmail.com>
2023-07-13 19:25:02 +0530

    Added dockerfile | Modified mongo connection string

--- config/config_dev.js
+++ config/config_dev.js
@@ -1,5 +1,5 @@
 const config = require('./config_global');
-config.mongo.connection_string = 'mongodb://127.0.0.1:27017/' + config.mongo.db;
+config.mongo.connection_string = 'mongodb://mongo:27017/' + config.mongo.db;
 config.hostname = 'http://localhost:8080';
 
 module.exports = config;
