45e31bca845b15ba08b1b55f68593e57f824eadf
Subramanya1702 <subramanyak1997@gmail.com>
2023-07-17 13:31:08 +0530

    Modified config files to use environment variables

--- config/config_dev.js
+++ config/config_dev.js
@@ -1,5 +1,8 @@
 const config = require('./config_global');
-config.mongo.connection_string = 'mongodb://mongo:27017/' + config.mongo.db;
-config.hostname = 'http://localhost:8080';
+config.hostname = `http://localhost:${config.port}`;
+
+if (process.env.DB_CONN_STR != null) {
+  config.mongo.connection_string = `mongodb://${process.env.DB_CONN_STR}/${config.mongo.db}`;
+}
 
 module.exports = config;
