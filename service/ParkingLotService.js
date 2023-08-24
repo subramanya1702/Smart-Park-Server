434eec5199f6e322072dd814bd4cee1427439a80
Subramanya1702 <subramanyak1997@gmail.com>
2023-08-24 12:33:49 +0530

    Temporary update to parking lots response - To be reverted

--- service/ParkingLotService.js
+++ service/ParkingLotService.js
@@ -13,12 +13,12 @@ exports.getAllRecentParkingLots = async () => {
     return recentParkingLots.map((parkingLot) => {
         return {
             id: parkingLot._id,
-            latitude: parkingLot.latitude,
-            longitude: parkingLot.longitude,
+            latitude: parseFloat(parkingLot.latitude),
+            longitude: parseFloat(parkingLot.longitude),
             parking_lot_name: parkingLot.name,
             number_of_empty_parking_slots: parkingLot.emptySpaces,
             total_number_of_parking_lots: parkingLot.totalSpaces,
-            timestamp: parkingLot.timestamp,
+            timestamp: timestampToFormattedDate(parkingLot.timestamp),
             image_url: `${config.hostname}/parking_lots/${parkingLot._id}/image`,
             parking_lot_time_limit: parkingLot.timeLimit,
             parking_charges: parkingLot.charges,
@@ -42,3 +42,16 @@ exports.getImageByParkingLotId = async (id) => {
 
     return parkingLot.ogImage.buffer;
 };
+
+
+function timestampToFormattedDate(timestamp) {
+    const date = new Date(timestamp * 1000);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
